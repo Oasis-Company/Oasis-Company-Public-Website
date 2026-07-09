779973c203750dcd4e71e5ff4ad4fd5c05a67821
CeaserZhao <chkevin.zhao@gmail.com>
2026-07-09 22:24:13 +0800

    update

---
+++ .wrangler/tmp/pages-JvTz8R/functionsRoutes-0.21547148609851852.mjs
@@ -0,0 +1,35 @@
+import { onRequest as __apply_api___catchall___js_onRequest } from "D:\\github projects\\Oasis Company Public Website\\functions\\apply\\api\\[[catchall]].js"
+import { onRequest as __apply_api__middleware_js_onRequest } from "D:\\github projects\\Oasis Company Public Website\\functions\\apply\\api\\_middleware.js"
+import { onRequest as __apply_js_onRequest } from "D:\\github projects\\Oasis Company Public Website\\functions\\apply.js"
+import { onRequest as __list_js_onRequest } from "D:\\github projects\\Oasis Company Public Website\\functions\\list.js"
+
+export const routes = [
+    {
+      routePath: "/apply/api/:catchall*",
+      mountPath: "/apply/api",
+      method: "",
+      middlewares: [],
+      modules: [__apply_api___catchall___js_onRequest],
+    },
+  {
+      routePath: "/apply/api",
+      mountPath: "/apply/api",
+      method: "",
+      middlewares: [__apply_api__middleware_js_onRequest],
+      modules: [],
+    },
+  {
+      routePath: "/apply",
+      mountPath: "/",
+      method: "",
+      middlewares: [],
+      modules: [__apply_js_onRequest],
+    },
+  {
+      routePath: "/list",
+      mountPath: "/",
+      method: "",
+      middlewares: [],
+      modules: [__list_js_onRequest],
+    },
+  ]
